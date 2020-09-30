66f6eb91bb9f602dcb09342ec007e53484f29284
flashultra <m_mail01@abv.bg>
2020-09-30 12:56:22 +0300

    Add function to translate standard UUIDs into shorter format and back

--- src/uuid/Uuid.hx
+++ src/uuid/Uuid.hx
@@ -1,5 +1,6 @@
 package uuid;
 
+import haxe.ds.Vector;
 import haxe.Timer;
 import haxe.Int64;
 import haxe.io.Bytes;
@@ -12,13 +13,25 @@ class Uuid {
 	public inline static var ISO_OID = '6ba7b812-9dad-11d1-80b4-00c04fd430c8';
 	public inline static var X500_DN = '6ba7b814-9dad-11d1-80b4-00c04fd430c8';
 	public inline static var NIL = '00000000-0000-0000-0000-000000000000';
+
+	public inline static var LOWERCASE_BASE26 = "abcdefghijklmnopqrstuvwxyz";
+	public inline static var UPPERCASE_BASE26 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";   
+	public inline static var NO_LOOK_ALIKES_BASE51 = "2346789ABCDEFGHJKLMNPQRTUVWXYZabcdefghijkmnpqrtwxyz"; // without 1, l, I, 0, O, o, u, v, 5, S, s
+	public inline static var FLICKR_BASE58 = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";  // without similar characters 0/O, 1/I/l
+	public inline static var BASE_70 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-+!@#$^";
+	public inline static var COOKIE_BASE90 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
 	
+	public inline static var NUMBERS_BIN = "01";
+	public inline static var NUMBERS_OCT = "01234567";
+	public inline static var NUMBERS_DEC = "0123456789";
+	public inline static var NUMBERS_HEX = "0123456789abcdef";
+
 
 	static var lastMSecs:Float = 0;
 	static var lastNSecs = 0;
 	static var clockSequenceBuffer:Int = -1;
 	static var regexp:EReg = ~/^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
-
+	
 	static var rndSeed:Int = Std.int(Timer.stamp() * 1000);
 	static var state0 = splitmix64_seed(rndSeed);
 	static var state1 = splitmix64_seed(rndSeed + 1);
@@ -45,7 +58,17 @@ class Uuid {
 		return randomFromRange(0, 255);
 	}
 
-	public static function v1(node:Bytes = null, optClockSequence:Int = -1, msecs:Float = -1, optNsecs:Int = -1, ?randomFunc:Void->Int, separator:String = "-"):String {
+	public static function fromShort(shortUuid:String, separator:String = '-', fromAlphabet:String = FLICKR_BASE58):String {
+		var uuid  = Uuid.convert(shortUuid,fromAlphabet,NUMBERS_HEX);
+		return hexToUuid(uuid,separator);
+	}
+
+	public static function toShort(uuid:String, separator:String = '-',toAlphabet:String = FLICKR_BASE58):String {
+		uuid = StringTools.replace(uuid, separator, '').toLowerCase();
+		return  Uuid.convert(uuid,NUMBERS_HEX,toAlphabet);
+	}
+
+	public static function v1(node:Bytes = null, optClockSequence:Int = -1, msecs:Float = -1, optNsecs:Int = -1, ?randomFunc:Void->Int, separator:String = "-", shortUuid:Bool = false, toAlphabet:String = FLICKR_BASE58):String {
 		if ( randomFunc == null) randomFunc = randomByte;
 		var buffer:Bytes = Bytes.alloc(16);
 		if (node == null) {
@@ -104,19 +127,22 @@ class Uuid {
 			buffer.set(i + 10, node.get(i));
 
 		var uuid = stringify(buffer,separator);
+		if ( shortUuid ) uuid = Uuid.toShort(uuid,separator,toAlphabet);
+
 		return uuid;
 	}
 
-	public static function v3(name:String, namespace:String = "", separator:String = "-"):String {
+	public static function v3(name:String, namespace:String = "", separator:String = "-", shortUuid:Bool = false, toAlphabet:String = FLICKR_BASE58):String {
 		namespace = StringTools.replace(namespace, '-', '');
 		var buffer = Md5.make(Bytes.ofHex(namespace + Bytes.ofString(name).toHex()));
 		buffer.set(6, (buffer.get(6) & 0x0f) | 0x30);
 		buffer.set(8, (buffer.get(8) & 0x3f) | 0x80);
 		var uuid = stringify(buffer,separator);
+		if ( shortUuid ) uuid = Uuid.toShort(uuid,separator,toAlphabet);
 		return uuid;
 	}
 
-	public static function v4(randBytes:Bytes = null, ?randomFunc:Void->Int, separator:String = "-"):String {
+	public static function v4(randBytes:Bytes = null, ?randomFunc:Void->Int, separator:String = "-", shortUuid:Bool = false, toAlphabet:String = FLICKR_BASE58):String {
 		if ( randomFunc == null) randomFunc = randomByte;
 		var buffer:Bytes = randBytes;
 		if ( buffer == null ) {
@@ -130,22 +156,22 @@ class Uuid {
 		buffer.set(6, (buffer.get(6) & 0x0f) | 0x40);
 		buffer.set(8, (buffer.get(8) & 0x3f) | 0x80);
 		var uuid = stringify(buffer,separator);
+		if ( shortUuid ) uuid = Uuid.toShort(uuid,separator,toAlphabet);
 		return uuid;
 	}
 
-	public static function v5(name:String, namespace:String = "", separator:String = "-"):String {
+	public static function v5(name:String, namespace:String = "", separator:String = "-", shortUuid:Bool = false, toAlphabet:String = FLICKR_BASE58):String {
 		namespace = StringTools.replace(namespace, '-', '');
 		var buffer = Sha1.make(Bytes.ofHex(namespace + Bytes.ofString(name).toHex()));
 		buffer.set(6, (buffer.get(6) & 0x0f) | 0x50);
 		buffer.set(8, (buffer.get(8) & 0x3f) | 0x80);
 		var uuid = stringify(buffer,separator);
+		if ( shortUuid ) uuid = Uuid.toShort(uuid,separator,toAlphabet);
 		return uuid;
 	}
 
 	public static function stringify(data:Bytes,separator:String = "-"):String {
-		var hex = data.toHex();
-		var uuid = hex.substr(0, 8) + separator + hex.substr(8, 4) + separator + hex.substr(12, 4) + separator + hex.substr(16, 4) + separator + hex.substr(20, 12);
-		return uuid;
+		return hexToUuid(data.toHex(),separator);
 	}
 
 	public static function parse(uuid:String, separator:String = "-"):Bytes {
@@ -165,4 +191,37 @@ class Uuid {
 		uuid = StringTools.replace(uuid, separator, '');
 		return Std.parseInt("0x"+uuid.substr(12,1));
 	}
+
+	private static function hexToUuid( hex:String , separator:String):String {
+		return ( hex.substr(0, 8) + separator + hex.substr(8, 4) + separator + hex.substr(12, 4) + separator + hex.substr(16, 4) + separator + hex.substr(20, 12));
+	}
+
+	public static function convert(number:String, fromAlphabet:String, toAlphabet:String ):String {
+        var fromBase:Int = fromAlphabet.length;
+        var toBase:Int  = toAlphabet.length;
+        var len = number.length;
+        var buf:String = "";
+        var numberMap:Vector<Int> = new Vector<Int>(len);
+        var divide:Int = 0, newlen:Int = 0;
+        for(i in 0...len) {
+            numberMap[i] = fromAlphabet.indexOf(number.charAt(i));
+        }
+        do {
+            divide = 0;
+            newlen = 0;
+            for(i in 0...len) {
+                divide = divide * fromBase + numberMap[i];
+                if (divide >= toBase) {
+                    numberMap[newlen++] = Math.floor( divide / toBase);
+                    divide = divide % toBase;
+                } else if (newlen > 0) {
+                    numberMap[newlen++] = 0;
+                }
+            }
+            len = newlen;
+            buf = toAlphabet.charAt(divide) + buf;
+        } while (newlen !=0 );
+
+        return buf;
+     }
 }
